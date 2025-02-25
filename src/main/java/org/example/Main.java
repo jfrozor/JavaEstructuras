3a5b1e9c62d27f259df07a8c33ab9f4a70aa8489
jfrozor <jfrozo@ucundinamarca.edu.co>
2025-02-25 12:45:43 -0500

    ejercicio en clase sentencias en Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,106 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
+            int menu=0, moneda=0, m20=0, m50=0, m100=0, m200=0,m500=0, suma=0;
+            Scanner teclado = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("dato");
+
+            do
+            {
+                System.out.println("MENU ALCANCÍA");
+                System.out.println("1 - INSERTAR MONEDAS");
+                System.out.println("2- CONTAR MONEDAS");
+                System.out.println("3- VALOR POR CADA DENOMINACIÓN");
+                System.out.println("4- SUMA");
+
+                menu=teclado.nextInt();
+
+                switch (menu)
+                {
+                    case 1:
+                        System.out.println("\n - INSERTAR MONEDA");
+                        System.out.println("INGRESA LA DENOMINACIÓN MONEDA 20, 50, 100, 200, 500");
+                        moneda= teclado.nextInt();
+                        switch (moneda)
+                        {
+                            case 20:
+                            {
+                                if(moneda==20)
+                                {m20++;}
+                            }
+                            break;
+
+                            case 50:
+                            {
+                                if(moneda==50)
+                                {m50++;}
+                            } break;
+
+                            case 100:
+                            {
+                                if(moneda==100)
+                                {m100++;}
+                            }
+                            break;
+                            case 200:
+                            {
+                                if(moneda==200)
+                                {m200++;}
+                            }
+                            break;
+
+                            case 500:
+                            {
+                                if(moneda==500)
+                                {m500++;}
+                            }
+                            break;
+                            default:
+                                System.out.println("LA DENOMINACIÓN INGRESADA NO ES VÁLIDA");
+                                break;
+                        }
+
+                        break;
+
+                    case 2:
+                        System.out.println("\n CANTIDAD MONEDAS POR DENOMINACIÓN");
+                        System.out.println("MONEDAS $20: "+m20);
+                        System.out.println("MONEDAS $50: "+m500);
+                        System.out.println("MONEDAS $100: "+m100);
+                        System.out.println("MONEDAS $200: "+m200);
+                        System.out.println("MONEDAS $500: "+m500);
+                        System.out.println("\n");
+
+                        break;
+
+                    case 3:
+
+                        System.out.println("\n SUBTOTALS POR DENOMINACIÓN");
+                        System.out.println("MONEDAS $20: "+m20*20);
+                        System.out.println("MONEDAS $50: "+m500*50);
+                        System.out.println("MONEDAS $100: "+m100*100);
+                        System.out.println("MONEDAS $200: "+m200*200);
+                        System.out.println("MONEDAS $500: "+m500*500);
+                        System.out.println("\n");
+
+                        break;
+                    case 4:
+                        suma=(m20*20)+(m50*50)+(m100*100)+(m200*200)+(m500*500);
+                        System.out.println("\n EL VALOR TOTAL DE MONEDAS ES: "+suma);
+                        System.out.println("\n");
+                        break;
+                    default:
+                        System.out.println("OPCIÓN NO VALIDA");
+                        break;
+                }
+            }
+
+            while(menu!=0);
+            System.out.println("CARACTER NO PERMITIDO");
+
         }
     }
-}
